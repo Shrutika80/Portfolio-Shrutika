86e4c663fba82a7dfc7a0138c9e5b7d220b2c796
Shrutika80 <sm77shruti@gmail.com>
2024-05-30 17:38:48 +0530

    Update the experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -43,21 +43,42 @@ const ListItem = styled.li`
   font-size: 16px;
 `;
 
+const ReadMoreButton = styled.button`
+  background: none;
+  border: none;
+  color: #007bff;
+  cursor: pointer;
+  padding: 0;
+  font-size: 1rem;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const workIcon = {
   icon: <FontAwesomeIcon icon={faBriefcase} />,
   iconStyle: { background: 'rgb(33, 150, 243)', color: '#fff' },
 };
 
-const Experience: React.FC = () => {  
+const Experience: React.FC = () => {
+  const [expanded, setExpanded] = useState<{ [key: number]: boolean }>({});
+
+  const toggleReadMore = (index: number) => {
+    setExpanded(prevState => ({ ...prevState, [index]: !prevState[index] }));
+  };
+  
     const timeline = [
     {
       icon: workIcon,
       date: 'June 2023 - Present',
       title: 'Senior Software Engineer',
       subtitle: 'Futops Technologies India Private Limited, Pune',
-      desc:  ['Drives innovation in front-end development, integrating new methodologies to improve performance, accessibility, and maintainability',
-      'The "Road Event Detection" project involves using a Leaflet map to display locations of cameras for monitoring road activity and real-time alerts for events such as traffic congestion',
-      'Implemented caching strategies to store frequently accessed map tiles locally, reducing the number of server requests and improving load times. Initial map load time decreased by 60%, from an average of 5 seconds to 2 seconds.'
+      desc:  ['Mentored and led a team of 2-3 junior developers, providing guidance on best practices for code integration and performance optimization.',
+      'Drive the adoption of new methodologies and technologies to enhance the performance, accessibility, and maintainability of front-end applications.',
+      'Lead the "Road Event Detection" project, utilizing a Leaflet map to display camera locations for monitoring road activity and providing real-time alerts for events such as traffic congestion.',
+      'Conduct regular code reviews to maintain high code quality and adherence to best practices.',
+      'Participate actively in Agile development processes, contributing to sprint planning, daily stand-ups, and retrospectives.',
+      'Engage with clients to understand their requirements and provide technical guidance and solutions to meet their needs'
       ],
     },
     {
@@ -65,9 +86,12 @@ const Experience: React.FC = () => {
       date: 'Nov 2020 - June 2023',
       title: 'Software Engineer',
       subtitle: 'Futops Technologies India Private Limited, Pune',
-      desc: ['Provided mentorship to junior, sharing best practices for working with code integration, and performance optimization. Conducted regular code reviews',
-      'The "People Tracking" project involves using Heatmap.js to display a heatmap that visualizes the density of people in different areas based on real-time data',
-      'Successfully integrated a feature using the company UI kit that allows users to draw and select specific rectangular areas on the map.'
+      desc: ['Collaborate with design and development teams to translate designs into responsive and interactive front-end code.',
+      'Utilize HTML5, CSS3, JavaScript, React.js, React Hooks, and React-Router to build and maintain user interfaces.',
+      'Work in Agile environments, participating in sprint planning, daily stand-ups, and retrospectives.',
+      'Continuously improve the codebase by adopting best practices for code efficiency and performance.',
+      'Conduct regular code reviews to ensure adherence to coding standards and best practices.',
+      'Interact with clients to gather requirements and provide technical solutions to meet their needs.'
       ],
     },
     {
@@ -75,41 +99,49 @@ const Experience: React.FC = () => {
       date: 'July 2019 - Jan 2020',
       title: 'Associate Software Engineer',
       subtitle: 'Infostretch solution Private Limited, Pune',
-      desc: ['Contributes to small, well-defined tasks and projects.',
-        'Have worked with web debugging tools (Chrome Developer Console).',
-        'Supports the team by fixing bugs and performing minor feature enhancements.'
+      desc: ['Develop customer-facing applications using modern front-end technologies including React.js, React Hooks, React-Router, JavaScript, Styled-components, HTML5, and CSS3.',
+        'Assist in the maintenance and support of existing applications by fixing bugs and implementing minor feature enhancements.',
+        'Write clean, maintainable, and efficient code adhering to best practices and coding standards.',
+        'Work collaboratively with senior developers and cross-functional teams to ensure smooth integration of front-end components.',
+        'Participate in team meetings, providing updates on task progress and discussing potential solutions to technical challenges.'
         ],
     }
   ];
 
   return (
     <ExperienceSection id='experience'>
-        <Heading>EXPERIENCE</Heading>
+      <Heading>EXPERIENCE</Heading>
       <VerticalTimeline>
         {timeline.map((t, i) => {
-            const arrowStyle = { borderRight: '7px solid  rgb(33, 150, 243)' }
-          
+          const arrowStyle = { borderRight: '7px solid  rgb(33, 150, 243)' }
+          const isExpanded = expanded[i] || false;
+
           return (
             <VerticalTimelineElement
               key={i}
               className="timeline__experience__vertical-timeline-element"
-                contentArrowStyle={arrowStyle}
+              contentArrowStyle={arrowStyle}
               {...t.icon}
               date={t.date}
             >
-              {t.title ? (
+              {t.title && (
                 <>
                   <Title>{t.title}</Title>
                   {t.subtitle && (
                     <Subtitle>{t.subtitle}</Subtitle>
                   )}
                   <List>
-                    {t.desc.map((item, idx) => (
+                    {t.desc.slice(0, isExpanded ? t.desc.length : 2).map((item, idx) => (
                       <ListItem key={idx}>{item}</ListItem>
                     ))}
                   </List>
+                  {t.desc.length > 2 && (
+                    <ReadMoreButton onClick={() => toggleReadMore(i)}>
+                      {isExpanded ? 'Read Less' : 'Read More'}
+                    </ReadMoreButton>
+                  )}
                 </>
-              ) : undefined}
+              )}
             </VerticalTimelineElement>
           );
         })}
